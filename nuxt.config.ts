f9092a59a8dd5fd5652e2b397a353096719e66dd
Cuong Ngo <dinhcuongngo@gmail.com>
2025-08-01 18:43:34 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['vuetify-nuxt-module'],
+  css: ['~/assets/css/styles.css'],
+  app: {
+    head: {
+      title: 'Home Massage',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Home Massage Service' },
+      ],
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Farsan&family=Lora:ital,wght@0,400..700;1,400..700&display=swap',
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        },
+      ],
+    },
+  },
+})
