301b4fa3f14c5a69be0406542fe7879ac782781d
Cuong Ngo <dinhcuongngo@gmail.com>
2025-08-01 21:20:27 +0700

    feat: install i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module'],
+  modules: ['vuetify-nuxt-module', '@nuxtjs/i18n'],
   css: ['~/assets/css/styles.css'],
   ssr: false,
   app: {
